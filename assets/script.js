b140d5d146dc71f856c6f61aeff9622da40bc0f0
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-08-06 11:39:55 -0700

    adding JS to make submit work for API call

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
+var requestedCity = '';
 
+$('#submitCityBtn').click(function(event) {
+    event.preventDefault();
+    requestedCity = $('#cityInput').val();
+    mainCityCall()
+})
 
 function mainCityCall () {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=47.43&lon=-122.10&appid=f510236949173fad67a61182bbdd1a37')
+    fetch('http://api.openweathermap.org/data/2.5//weather?appid=f510236949173fad67a61182bbdd1a37&q='+requestedCity+'&units=imperial')
         .then((response) => {
             return response.json();
     })
@@ -10,4 +16,7 @@ function mainCityCall () {
     });
 };
 
-mainCityCall();
+
+//
+
+// https://api.openweathermap.org/data/2.5/onecall?lat=47.43&lon=-122.10&exclude=hourly,minutely&appid=f510236949173fad67a61182bbdd1a37&units=imperial
