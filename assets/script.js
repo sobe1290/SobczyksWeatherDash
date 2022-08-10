6f083397155abaffe580f57899fde276b563f3e5
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-08-09 22:11:52 -0700

    adding UV and Icon to card

--- assets/script.js
+++ assets/script.js
@@ -9,59 +9,44 @@ $('#submitCityBtn').click(function(event) {
     // historyCity.classList = 'list-item flex-row justify-space-between align-center';
     // historyCity.setText('text',;
 
-})
+});
 
 function mainCityCall () {
     fetch('http://api.openweathermap.org/data/2.5//weather?appid=f510236949173fad67a61182bbdd1a37&q='+requestedCity+'&units=imperial')
         .then((response) => {
             return response.json();
-    })
+        })
         .then(function (myJson) {
-            console.log(myJson);
-            const {name} = myJson;
-            $("#currentCityName").text(name);
+            console.log(myJson);       
+            const {name} = myJson;            
             const {dt} = myJson;
             const {timezone} = myJson;
-            var adjustedTime = timezone / 60;
-            var formattedTime = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
-            $("#cityDateTime").text(formattedTime);
+            var adjustedTime = timezone / 60; 
+            var formattedTime = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');          
             const {temp} = myJson.main;
-            $("#currentTemp").text(temp + "°F");
             const {speed} = myJson.wind;
-            $('#currentWindSpeed').text(speed+"km/h");
             const {humidity} = myJson.main;
-            $('#currentHumid').text(humidity+"%");
             const {lat} = myJson.coord;
-            const {lon} = myJson.coord;
-            
+            const {lon} = myJson.coord; 
+            const {icon} = myJson.weather[0]
+            $('#currentHumid').text(humidity+"%");
+            $("#currentCityName").text(name);
+            $("#cityDateTime").text(formattedTime);
+            $("#currentTemp").text(temp + "°F");
+            $('#currentWindSpeed').text(speed+"km/h"); 
+            $('#weatherIcon').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png')         
             return secondCityCall(lat, lon)
-            
-            //waiting for API key to be activated before testing again
-
-        })
-
+        });
 };
 
 function secondCityCall (lat, lon) {
     fetch('http://api.openweathermap.org/data/2.5/onecall?appid=f510236949173fad67a61182bbdd1a37&lat='+ lat +'&lon='+ lon +'&exclude=hourly,daily')
-    .then((response) => {
-        return response.json();
-    })
-    .then(function (myJson) {
-        console.log(myJson);
-        const {name} = myJson;
-        $("#currentCityName").text(name);
-        const {dt} = myJson;
-        const {timezone} = myJson;
-        var adjustedTime = timezone / 60;
-        var formattedTime = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
-        $("#cityDateTime").text(formattedTime);
-        const {temp} = myJson.main;
-        $("#currentTemp").text(temp + "°F");
-        const {speed} = myJson.wind;
-        $('#currentWindSpeed').text(speed+"km/h");
-        const {humidity} = myJson.main;
-        $('#currentHumid').text(humidity+"%");
-        const {lat} = myJson.coord;
-        const {lon} = myJson.coord;
-}
+        .then((response) => {
+            return response.json();
+        })
+        .then(function (secondJson) {
+            console.log(secondJson); 
+            const {uvi} = secondJson.current;
+            $('#currentUV').text(uvi);
+        });       
+};
