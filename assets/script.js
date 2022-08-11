5ac7d06db57fc8a2a93bd776a04c10afa70c0264
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-08-10 19:58:36 -0700

    cards fully functional

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,7 @@ function mainCityCall () {
             $("#currentCityName").text(name);
             $("#cityDateTime").text(formattedTime);
             $("#currentTemp").text(temp + "°F");
-            $('#currentWindSpeed').text(speed+"km/h"); 
+            $('#currentWindSpeed').text(speed+" MPH"); 
             $('#weatherIcon').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png') ;
 
             //console.log(moment(formattedTime, "MM/DD/YYYY").add(1, 'days'));       
@@ -62,53 +62,83 @@ function secondCityCall (lat, lon) {
             };
 
             (function() {
-            const {dt} = secondJson.daily[0];
+            const {dt} = secondJson.daily[1];
             const {timezone_offset} = secondJson;
-            const {icon} = secondJson.daily[0].weather[0]
+            const {icon} = secondJson.daily[1].weather[0];
+            const {day} = secondJson.daily[1].temp;
+            const {wind_speed} = secondJson.daily[1];
+            const {humidity} = secondJson.daily[1];
             var adjustedTime = timezone_offset / 60; 
             var forecastday1 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
             $('#forecastDay1').text(forecastday1);
-            $('#weatherIconDay1').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png')
+            $('#weatherIconDay1').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png');
+            $("#day1Temp").text("Temp: "+ day + "°F");
+            $('#day1WindSpeed').text("Wind Speed: "+ wind_speed +" MPH"); 
+            $('#day1Humid').text("Humidity: "+ humidity +"%");
             })();
-
-            (function() {
-                const {dt} = secondJson.daily[1];
-                const {timezone_offset} = secondJson;
-                const {icon} = secondJson.daily[1].weather[0]
-                var adjustedTime = timezone_offset / 60; 
-                var forecastday2 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
-                $('#forecastDay2').text(forecastday2);
-                $('#weatherIconDay2').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png')
-                })();
 
             (function() {
                 const {dt} = secondJson.daily[2];
                 const {timezone_offset} = secondJson;
-                const {icon} = secondJson.daily[2].weather[0]
+                const {icon} = secondJson.daily[2].weather[0];
+                const {day} = secondJson.daily[2].temp;
+                const {wind_speed} = secondJson.daily[2];
+                const {humidity} = secondJson.daily[2];
                 var adjustedTime = timezone_offset / 60; 
-                var forecastday3 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
-                $('#forecastDay3').text(forecastday3);
-                $('#weatherIconDay3').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png')
+                var forecastday2 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
+                $('#forecastDay2').text(forecastday2);
+                $('#weatherIconDay2').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png');
+                $("#day2Temp").text("Temp: "+ day + "°F");
+                $('#day2WindSpeed').text("Wind Speed: "+ wind_speed +" MPH");
+                $('#day2Humid').text("Humidity: "+ humidity +"%");
                 })();
 
             (function() {
                 const {dt} = secondJson.daily[3];
                 const {timezone_offset} = secondJson;
-                const {icon} = secondJson.daily[3].weather[0]
+                const {icon} = secondJson.daily[3].weather[0];
+                const {day} = secondJson.daily[3].temp;
+                const {wind_speed} = secondJson.daily[3];
+                const {humidity} = secondJson.daily[3];
                 var adjustedTime = timezone_offset / 60; 
-                var forecastday4 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
-                $('#forecastDay4').text(forecastday4);
-                $('#weatherIconDay4').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png')
+                var forecastday3 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
+                $('#forecastDay3').text(forecastday3);
+                $('#weatherIconDay3').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png');
+                $("#day3Temp").text("Temp: "+ day + "°F");
+                $('#day3WindSpeed').text("Wind Speed: "+ wind_speed +" MPH");
+                $('#day3Humid').text("Humidity: "+ humidity +"%");
                 })();
 
             (function() {
                 const {dt} = secondJson.daily[4];
                 const {timezone_offset} = secondJson;
-                const {icon} = secondJson.daily[4].weather[0]
+                const {icon} = secondJson.daily[4].weather[0];
+                const {day} = secondJson.daily[4].temp;
+                const {wind_speed} = secondJson.daily[4];
+                const {humidity} = secondJson.daily[4];
+                var adjustedTime = timezone_offset / 60; 
+                var forecastday4 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
+                $('#forecastDay4').text(forecastday4);
+                $('#weatherIconDay4').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png');
+                $("#day4Temp").text("Temp: "+ day + "°F");
+                $('#day4WindSpeed').text("Wind Speed: "+ wind_speed +" MPH");
+                $('#day4Humid').text("Humidity: "+ humidity +"%");
+                })();
+
+            (function() {
+                const {dt} = secondJson.daily[5];
+                const {timezone_offset} = secondJson;
+                const {icon} = secondJson.daily[5].weather[0];
+                const {day} = secondJson.daily[5].temp;
+                const {wind_speed} = secondJson.daily[5];
+                const {humidity} = secondJson.daily[5];
                 var adjustedTime = timezone_offset / 60; 
                 var forecastday5 = moment.unix(dt).utc().utcOffset(adjustedTime).format('MM/DD/YYYY');
                 $('#forecastDay5').text(forecastday5);
-                $('#weatherIconDay5').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png')
+                $('#weatherIconDay5').attr("src",'http://openweathermap.org/img/wn/'+ icon +'.png');
+                $("#day5Temp").text("Temp: "+ day + "°F");
+                $('#day5WindSpeed').text("Wind Speed: "+ wind_speed +" MPH");
+                $('#day5Humid').text("Humidity: "+ humidity +"%");
                 })();
 
         });      
